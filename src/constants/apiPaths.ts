50eec5eaab26d9d82cb29850723bb0f7a70e3010
Kerwyn Rojas <kerwyn_rojas@epam.com>
2023-03-16 22:12:10 -0600

    Implemented upload file.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,11 @@
 const BASE_PATH = "https://6me96m3are.execute-api.eu-west-1.amazonaws.com/dev";
+const IMPORT_BASE_PATH =
+  "https://cgn3sjbb2a.execute-api.us-west-1.amazonaws.com/dev";
 
 const API_PATHS = {
   product: `${BASE_PATH}/products`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: IMPORT_BASE_PATH,
   bff: `${BASE_PATH}/products`,
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
