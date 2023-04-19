bca7d69b7e1472e1acbb44f0582c823c91dc3f84
Kerwyn Rojas <kerwyn_rojas@epam.com>
2023-04-18 21:11:28 -0600

    Task 6 adapted urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,13 +1,13 @@
-const BASE_PATH = "https://6me96m3are.execute-api.eu-west-1.amazonaws.com/dev";
+const BASE_PATH = "https://dbxxlpoykj.execute-api.us-west-1.amazonaws.com/dev";
 const IMPORT_BASE_PATH =
-  "https://cgn3sjbb2a.execute-api.us-west-1.amazonaws.com/dev";
+  "https://g8scehlcli.execute-api.us-west-1.amazonaws.com/dev";
 
 const API_PATHS = {
   product: `${BASE_PATH}/products`,
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://.execute-api.us-west-1.amazonaws.com/dev",
   import: IMPORT_BASE_PATH,
   bff: `${BASE_PATH}/products`,
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.us-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
